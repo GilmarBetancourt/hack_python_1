0a45a68c7b3a7b68cb5bb6e463dc776b15e37832
GilmarBetancourt <gilmarbeta@hotmail.com>
2025-03-28 19:13:55 -0400

    Se completaron todos los hacks y todos los tests fueron pasados.

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,7 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
+    result = result.upper().replace("O", "0", 2).replace("I", "1").replace("A", "@")
+    result = list(result)
     return result  
+
